b7d6df508be10e4146437c30718c74186a8e5172
Oliver <icebear1981@gmail.com>
2022-11-16 16:03:45 +0100

    ?

---
+++ models.py
@@ -0,0 +1,19 @@
+class Kunden(models.Model):
+    kdnr = models.IntegerField(primary_key=True)
+    kname = models.CharField(max_length=70)
+    kvname = models.CharField(max_length=70)
+    kstr = models.CharField(max_length=70)
+    khausnr = models.IntegerField()
+    kplz = models.IntegerField()
+    kort = models.CharField(max_length=70)
+
+class Artikel(models.Model):
+    artnr = models.IntegerField(primary_key=True)
+    artbez = models.CharField(max_length=70)
+    artpreis = models.CharField(max_length=70)
+
+class Orders(models.Model):
+    onr = models.IntegerField(primary_key=True)
+    kdnr = models.ForeignKey(Kunden)
+    anr = models.ForeignKey(Artikel)
+    aanz = models.Integer_Field()
